406d56d1c54be68bb99fe1b139100eedeaebd318
Muhammad Shafeen <112253449+shafeenyousafzaii@users.noreply.github.com>
2024-07-24 15:09:45 +0500

    Yolo8n
    I grabbed on the Yolo8n model for object detection later i will be modifying it further for more optimization and demand fulfillment

--- YOLOv9-Object-Detector-Android-Tflite/app/src/main/java/com/surendramaran/yolov9tflite/Constants.kt
+++ YOLOv9-Object-Detector-Android-Tflite/app/src/main/java/com/surendramaran/yolov9tflite/Constants.kt
@@ -1,6 +1,6 @@
 package com.surendramaran.yolov9tflite
 
-object Constants {
-    const val MODEL_PATH = "v9.tflite"
+object  Constants {
+    const val MODEL_PATH = "yolov8n_float32.tflite"
     const val LABELS_PATH = "labels.txt"
 }
